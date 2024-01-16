fa7d82aad59cadb5aaf96b232c75a0eb48d763fe
Colin <cbarkwith@hotmail.com>
2024-01-16 13:52:37 +0000

    First bar chart produced

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Bar } from "react-chartjs-2";
 import "./App.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from "chart.js";
 
-function App() {
-  return <></>;
-}
+ChartJS.register(CategoryScale, LinearScale, BarElement);
+
+const App = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "Sales",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return <Bar data={data} />;
+};
 
 export default App;
